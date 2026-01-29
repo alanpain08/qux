caddeb50f5385046f98efc7f321f72017707a6c8
Alan Tsarakov <alantsarakov@yandex.ru>
2026-01-29 10:22:15 +0300

    add limits for logs, refactor logger

--- src/logger.js
+++ src/logger.js
@@ -1,11 +1,10 @@
 const pino = require('pino');
 
+const isDev = process.env.NODE_ENV == 'development';
+
 const logger = pino({
-  transport:
-    process.env.NODE_ENV === 'development'
-      ? { target: 'pino-pretty', options: { colorize: true } }
-      : undefined,
-  level: process.env.LOG_LEVEL ?? 'info',
+  transport: isDev ? { target: 'pino-pretty', options: { colorize: true } } : undefined,
+  level: process.env.LOG_LEVEL || (isDev ? 'debug' : 'info'),
 });
 
 module.exports = logger;
